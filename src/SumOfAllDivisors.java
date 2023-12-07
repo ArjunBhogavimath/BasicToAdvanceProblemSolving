653c5a4f298c155dc2aaa4d787c10e72069d93bd
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2023-12-08 01:52:02 +0530

    Sum of all divisors

---
+++ src/SumOfAllDivisors.java
@@ -0,0 +1,13 @@
+//Sum of all divisors
+// coding ninjas
+//1.4 Maths
+
+public class SumOfAllDivisors {
+    public static int sumOfAllDivisors(int n){
+        // Write your code here.
+        int sum = 0;
+        for (int i = 1; i <= n; ++i)
+            sum += (n / i) * i;
+        return sum;
+    }
+}

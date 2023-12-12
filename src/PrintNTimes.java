bd1c4ffc5c2155f2fb2cfb9d6437dcc3c4f2215a
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2023-12-13 01:54:42 +0530

    1.5 Recursion 1 to N array

---
+++ src/PrintNTimes.java
@@ -0,0 +1,19 @@
+//print N times string
+//CODING NINJAS
+//1.5 BASIC RECURSION STRIVER
+public class PrintNTimes {
+    public static void printNtimes(int n){
+        // Write your code here.
+        if(n<1){
+            return;
+        }
+        System.out.print("Coding Ninjas ");
+        n--;
+        printNtimes(n);
+    }
+}
+
+//declare basecase properly
+//if the n becomes less than 1 we should return
+//otherwise print the string and decrement n
+//and call the function again

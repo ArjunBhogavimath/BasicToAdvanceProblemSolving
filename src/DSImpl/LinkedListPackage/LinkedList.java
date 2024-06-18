7e3a90936b38d7c6dc360835ffde29b41913c32d
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2024-06-19 00:16:56 +0530

    Array to linkedlist conversion

--- src/DSImpl/LinkedListPackage/LinkedList.java
+++ src/DSImpl/LinkedListPackage/LinkedList.java
@@ -17,14 +17,51 @@ class Node {
         this.data = data;
         this.next  = null; //the last element will be pointing towards null
     }
+
+
 }
 
 public class LinkedList {
 
     public static void main(String[] args) {
         int[] arr = {2,4,6,3};
-        Node node = new Node(arr[3]);
-        System.out.println(node.data);
-        System.out.println(node.next);
+//        Node node = new Node(arr[3]);
+//        System.out.println(node.data);
+//        System.out.println(node.next);
+        Node head = convertArrToLL(arr);
+        System.out.println(head.data);
+    }
+
+    public static Node convertArrToLL(int[] arr){
+        //mark the head as arr[0] by creating new node 1st
+        Node head = new Node(arr[0]);
+
+        //create mover variable to link head to next element
+        Node mover = head;
+
+        for(int i=1;i<arr.length;i++){
+            //create temp variable to store the elements and link to previous one
+            Node temp = new Node(arr[i]);
+            //now link mover next to temp
+            mover.next = temp;
+            //now move the mover to last element
+            mover = temp;
+        }
+        return head;
     }
 }
+
+
+/*
+Basic notes :
+Memory allocation :
+32-bit -->
+int -> 4 bytes
+next -> 4 bytes
+
+64-bit -->
+int -> 4 bytes
+next -> 8 bytes
+
+TC FOR ADDING ARRAY -> O(N)
+ */
